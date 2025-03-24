d907685525b847ee65bf00c3e5230b0220ef5ebf
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-24 18:56:47 +1000

    Fix vim-tmux-navigator

--- flake.nix
+++ flake.nix
@@ -2,41 +2,43 @@
   description = "NewDawn0's tmux config";
 
   inputs.utils.url = "github:NewDawn0/nixUtils";
+
   outputs = { self, utils, ... }: {
     overlays.default = final: prev: {
       ndtmux = self.packages.${prev.system}.default;
     };
     packages = utils.lib.eachSystem { } (pkgs:
       let
-        tmuxPlugins = pkgs.symlinkJoin {
+        plugins = with builtins; readDir ./plugins |> attrNames;
+        tmuxPlugins = pkgs.stdenvNoCC.mkDerivation {
           name = "tmux-plugins";
-          paths = with pkgs.tmuxPlugins; [ vim-tmux-navigator yank ];
-        };
-        tmuxCfg = pkgs.stdenvNoCC.mkDerivation {
-          name = "tmux-cfg";
           src = ./.;
           dontBuild = true;
-          installPhase = "install -D tmux.conf $out/lib/tmux.conf";
+          dontConfigure = true;
+          installPhase = ''
+            mkdir -p $out/share
+            cp -R plugins $out/share/
+          '';
         };
-        tmuxBin = pkgs.writeShellScriptBin "tmux" ''
-          ${pkgs.tmux}/bin/tmux
-        '';
-        tmuxFinal = pkgs.symlinkJoin {
+        tmuxCfg = pkgs.writeTextFile {
+          name = "tmux-cfg";
+          text = with builtins;
+            readFile ./tmux.conf + concatStringsSep "\n"
+            (map (p: "source-file ${tmuxPlugins}/share/plugins/${p}") plugins);
+          destination = "/share/tmux.conf";
+        };
+      in {
+        default = pkgs.writeShellApplication {
           name = "tmux";
-          paths = [ tmuxBin tmuxCfg ];
+          text = "${pkgs.tmux}/bin/tmux -f ${tmuxCfg}/share/tmux.conf";
+          meta = {
+            description = "Fully setup runnable tmux configuration";
+            homepage = "https://github.com/NewDawn0/tmuxConfig";
+            license = pkgs.lib.licenses.mit;
+            maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+            platforms = pkgs.lib.platforms.all;
+          };
         };
-        # tmuxBin = pkgs.writeShellApplication {
-        #   name = "tmux";
-        #   text = "${pkgs.tmux}/bin/tmux -f ${tmuxCfg}/lib/tmux.conf";
-        #   meta = {
-        #     description = "Fully setup runnable tmux configuration";
-        #     homepage = "https://github.com/NewDawn0/tmuxConfig";
-        #     license = pkgs.lib.licenses.mit;
-        #     maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-        #     platforms = pkgs.lib.platforms.all;
-        #   };
-        # };
-      in { default = tmuxFinal; });
-
+      });
   };
 }
